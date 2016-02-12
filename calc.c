899761709b6d2c3691a94f318351420645acb5b3
programmer55 <micro1130@outlook.com>
2016-02-11 20:15:55 -0500

    Completed add, subtract and multiply functions.

--- calc.c
+++ calc.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <string.h>
 
@@ -12,32 +11,50 @@ int main(){
 	int add(int,int);
 	int subt(int,int);
 
-	//asks the user which MATHS they want to do
-	printf("Do: add, subtract or multiply \n");
 	//takes user input and places it in string array user_input
 	scanf("%s", user_input1);
 
+	printf("Choice user picked: %s \n", user_input1);
+
 	//checks to see user_input value which indicates what MATHS the USER wants to do
 	//then it calls the proper function after getting number values from USER
-	if(strcmp(user_input1, "add")){
+	if(strcmp(user_input1, "add") == 0){
 		printf("Please input two numbers: \n");
 		scanf("%d", &first_num);
 		scanf("%d", &second_num);
-		int add();
-	} else if(strcmp(user_input1, "multiply")) {
+		add(first_num, second_num);
+	} else if(strcmp(user_input1, "multiply") == 0) {
 		scanf("%d", &first_num);
 		scanf("%d", &second_num);
-		int mult();
-	} else if(strcmp(user_input1, "subtract")){
+		mult(first_num, second_num);
+	} else if(strcmp(user_input1, "subtract") == 0){
 		scanf("%d", &first_num);
 		scanf("%d", &second_num);
-		int subt();
+		subt(first_num, second_num);
 	} else {
 		printf("Please enter either: add, subtract or multiply.");
 	}
 
-	printf("%d \n", first_num);
-	printf("%d", second_num);
+	return 0;
+}
+
+int add(number1, number2){
+	int result = number1 + number2;
+	printf("Result: %d", result);
+
+	return 0;
+}
+
+int mult(number1, number2){
+	int result = number1 * number2;
+	printf("Result: %d", result);
+
+	return 0;
+}
+
+int subt(number1, number2){
+	int result = number1 - number2;
+	printf("Result: %d", result);
 
 	return 0;
 }
